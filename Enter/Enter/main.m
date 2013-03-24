85e73d987866e52f463513bacb8c767416551158
Yohei Yamaguchi <joker13meister@gmail.com>
2013-03-25 00:10:34 +0900

    finished until the 1st chapter

---
+++ Enter/Enter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Enter
+//
+//  Created by Yohei Yamaguchi on 2013/03/24.
+//  Copyright (c) 2013年 Yohei Yamaguchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "StudyAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([StudyAppDelegate class]));
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-ios-practice
+35 Tips and Points in the iPhone Applications Development
 ============
+
+## Progress
+
+- 0th chapter. done on Mar. 25, 2013.
+- 1st chapter. done on Mar. 25, 2013.
+
+## Link
+
+- [Official Page](http://www.shuwasystem.co.jp/products/7980html/3323.html)
+- [Download Sample Codes](http://www.shuwasystem.co.jp/support/7980html/3323.html)
