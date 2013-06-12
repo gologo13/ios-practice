b887d950791a8cfa23d0401a0b7705ea902fea20
Yohei Yamaguchi <joker13meister@gmail.com>
2013-06-13 00:10:25 +0900

    finished the 25th chapter

---
+++ Book/ThumbnailWithScroll/Thumbnail/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  Thumbnail
+//
+//  Created by Yohei Yamaguchi on 2013/06/02.
+//  Copyright (c) 2013年 Yohei Yamaguchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ThumbnailView.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate,ThumbnailViewDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
